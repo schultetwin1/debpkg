fc1868af03316d29dc4eea1a779fca04ca399042
Matt Schulte <schultetwin1@gmail.com>
2019-10-28 23:25:08 -0700

    Adding more tests to control

--- src/control.rs
+++ src/control.rs
@@ -8,6 +8,7 @@ use regex::Regex;
 
 type Paragraph = std::collections::HashMap<String, Vec<String>>;
 
+#[derive(Debug)]
 pub struct Control {
     paragraph: Paragraph
 }
@@ -26,7 +27,7 @@ impl Control {
         let comment_regex = Regex::new(r"^#.*$").unwrap();
         let continuation_regex = Regex::new(r"^\s+(?P<continuation>\S.*)$").unwrap();
         let paragraph_sep_regex = Regex::new(r"^\s*$").unwrap();
-        let field_regex = Regex::new(r"(?P<field_name>\w+):(?P<field_value>.*)$").unwrap();
+        let field_regex = Regex::new(r"^(?P<field_name>\w+):(?P<field_value>.*)$").unwrap();
 
         let mut curr_name: String = String::default();
 
@@ -58,6 +59,9 @@ impl Control {
             }
 
             if let Some(captures) = continuation_regex.captures(&line) {
+                if curr_name.is_empty() {
+                    return Err(Error::InvalidControlFile);
+                }
                 let continuation = captures.name("continuation").unwrap().as_str();
                 let data = ctrl.paragraph.get_mut(&curr_name).unwrap();
                 data.push(continuation.to_owned());
@@ -78,11 +82,18 @@ impl Control {
     }
 
     pub fn name(&self) -> &str {
-        &self.paragraph.get("package").unwrap()[0]
+        self.get("Package").unwrap()
     }
 
     pub fn version(&self) -> &str {
-        &self.paragraph.get("version").unwrap()[0]
+        self.get("Version").unwrap()
+    }
+
+    pub fn get(&self, field_name: &str) -> Option<&str> {
+        match self.paragraph.get(field_name.to_lowercase().as_str()) {
+            Some(lines) => Some(&lines[0]),
+            None => None
+        }
     }
 }
 
@@ -95,5 +106,41 @@ mod tests {
     #[test]
     fn empty_control_file_fails () {
         assert!(Control::parse("".as_bytes()).is_err());
+    }
+
+    #[test]
+    fn only_name_fails_parse() {
+        let err = Control::parse("package: name_only".as_bytes()).unwrap_err();
+        assert_matches!(err, Error::MissingPackageVersion);
+    }
+
+    #[test]
+    fn only_version_fails_parse() {
+        let err = Control::parse("version: 1.8.2".as_bytes()).unwrap_err();
+        assert_matches!(err, Error::MissingPackageName);
+    }
+
+    #[test]
+    fn name_and_version_parse() {
+        let ctrl = Control::parse("package: name\nversion: 1.8.2".as_bytes()).unwrap();
+        assert!(ctrl.name() == "name");
+        assert!(ctrl.version() == "1.8.2");
+    }
+
+    #[test]
+    fn proper_description_parse() {
+        let ctrl = Control::parse("package: name\nversion: 1.8.2\nDescription: short\n very\n long".as_bytes()).unwrap();
+        assert!(ctrl.name() == "name");
+        assert!(ctrl.version() == "1.8.2");
+        let desc = ctrl.paragraph.get("description").unwrap();
+        assert!(desc[0] == "short");
+        assert!(desc[1] == "very");
+        assert!(desc[2] == "long");
+    }
+
+    #[test]
+    fn control_starting_with_continuation_fails() {
+        let err = Control::parse(" continue\npackage: name\nversion: 1.8.2".as_bytes()).unwrap_err();
+        assert_matches!(err, Error::InvalidControlFile);
     }
 }
